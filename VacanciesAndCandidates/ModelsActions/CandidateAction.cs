6ac01a3245cd42af34da2dfaae3370b9403260e3
Елена Габова <Eleyurgab@gmail.com>
2021-09-15 23:35:44 +0300

    -

---
+++ VacanciesAndCandidates/ModelsActions/CandidateAction.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacanciesAndCandidates.DatabaseSettings.CandidateContext;
+using VacanciesAndCandidates.Models.CandidateModel;
+
+namespace VacanciesAndCandidates.ModelsActions.CandidateAction
+{
+    static class CandidateAction
+    {
+        public static void AddCandidate(string[] sqlParameters,
+                                        bool gender,
+                                        out bool inputCorrect,
+                                        out string resultText)
+        {
+
+            var inputCorrectParam = new SqlParameter
+            {
+                ParameterName = "inputCorrect",
+                DbType = System.Data.DbType.Boolean,
+                Direction = System.Data.ParameterDirection.Output
+            };
+            var resultTextParam = new SqlParameter
+            {
+                ParameterName = "resultText",
+                DbType = System.Data.DbType.String,
+                Size = 250,
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            using (CandidateContext db = new CandidateContext())
+            {
+                string addCandidateQuery = string.Format(
+                    @"EXECUTE [dbo].[AddCandidate] 
+                       '{0}',
+                       '{1}',
+                       @inputCorrect OUTPUT,
+                       @resultText OUTPUT", String.Join("',\n'", sqlParameters), gender);
+
+                var addCandidateQueryResult = db.Database.ExecuteSqlCommand(addCandidateQuery, inputCorrectParam, resultTextParam);
+                db.SaveChanges();
+
+                inputCorrect = (bool)inputCorrectParam.Value;
+                resultText = resultTextParam.Value.ToString();
+            }
+        }
+
+        public static ObservableCollection<CandidateModel> GetCandidate()
+        {
+            var candidateList = new ObservableCollection<CandidateModel>();
+
+            using (CandidateContext db = new CandidateContext())
+            {
+                foreach (var item in db.Candidates.ToList()) 
+                    candidateList.Add(item);
+            }
+
+            return candidateList;
+        }
+    }
+}
---
+++ VacanciesAndCandidates/Views/CreateCandidateWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VacanciesAndCandidates.ModelsActions.CandidateAction;
+using VacanciesAndCandidates.Models.CandidateModel;
+
+namespace VacanciesAndCandidates
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class CreateCandidateWindow : Window
+    {
+        public CandidateModel Candidate;
+
+        public CreateCandidateWindow()
+        {
+            InitializeComponent();
+        }
+        public CreateCandidateWindow(CandidateModel c)
+        {
+            InitializeComponent();
+            Candidate = c;
+
+            FullName.Text = Candidate.First_name + ' ' + Candidate.Last_name;
+            if (!string.IsNullOrEmpty(Candidate.Middle_name))
+                FullName.Text += ' ' + Candidate.Middle_name;
+
+            Email.Text = Candidate.Email;
+            dt_birthday.Text = Convert.ToString(Candidate.Dt_birthday);
+            gender.IsChecked = Candidate.Gender;
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void CreateCadidate_Click(object sender, RoutedEventArgs e)
+        {
+            var partsOfFullName = FullName.Text.Split(' ');
+
+            string first_name  = partsOfFullName[0];
+            string last_name   = partsOfFullName.Count() > 1 ? partsOfFullName[1] : string.Empty;
+            string middle_name = partsOfFullName.Count() > 2 ? partsOfFullName[2] : string.Empty;
+            
+            string[] sqlParameters = new string[] { first_name,
+                                                    last_name,
+                                                    middle_name,
+                                                    Email.Text, 
+                                                    dt_birthday.Text };
+
+            bool inputNotCorrect = false;
+            string resultText = string.Empty;
+            
+            CandidateAction.AddCandidate(sqlParameters,
+                                         (bool)gender.IsChecked,
+                                         out inputNotCorrect,
+                                         out resultText);
+
+            var textColor = Colors.Red;
+            if (!inputNotCorrect)
+            {
+                FullName.Text = string.Empty;
+                Email.Text    = string.Empty;
+                dt_birthday.Text = string.Empty;
+                gender.IsChecked = true;
+                textColor = Colors.Green ;
+            }
+
+            infoTextBlock.Text = resultText;
+            infoTextBlock.Foreground = new SolidColorBrush(textColor);
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
